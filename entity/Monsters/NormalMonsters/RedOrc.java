cfcb12199a678bd2ec8b17a3a6d1c6a54ffe7b73
SLarice <stephane.larice@etu.univ-amu.fr>
2024-01-09 19:17:49 +0100

    new file:   Player/Monster/BlueOrc/down1.png 	new file:   Player/Monster/BlueOrc/down2.png 	new file:   Player/Monster/BlueOrc/left1.png 	new file:   Player/Monster/BlueOrc/left2.png 	new file:   Player/Monster/BlueOrc/right1.png 	new file:   Player/Monster/BlueOrc/right2.png 	new file:   Player/Monster/BlueOrc/up1.png 	new file:   Player/Monster/BlueOrc/up2.png 	new file:   Player/Monster/RedOrc/down1.png 	new file:   Player/Monster/RedOrc/down2.png 	new file:   Player/Monster/RedOrc/left1.png 	new file:   Player/Monster/RedOrc/left2.png 	new file:   Player/Monster/RedOrc/right1.png 	new file:   Player/Monster/RedOrc/right2.png 	new file:   Player/Monster/RedOrc/up1.png 	new file:   Player/Monster/RedOrc/up2.png 	new file:   entity/Monsters/NormalMonsters/BlueOrc.java 	new file:   entity/Monsters/NormalMonsters/RedOrc.java 	modified:   entity/Abilities/FireBall/FireBall.java 	modified:   entity/Entity.java 	modified:   entity/Monsters/Monster.java 	modified:   entity/Monsters/NormalMonsters/Orc.java 	modified:   main/main/GamePannel.java

---
+++ entity/Monsters/NormalMonsters/RedOrc.java
@@ -0,0 +1,40 @@
+package entity.Monsters.NormalMonsters;
+
+import main.GamePannel;
+import entity.Monsters.Monster;
+import entity.Monsters.MonsterInterface;
+
+public class RedOrc extends Monster implements MonsterInterface {
+    
+    public RedOrc (GamePannel gp, String inputedDirection, int coordX, int coordY) {
+        super(gp, inputedDirection, coordX, coordY);
+
+        getThisMonsterImage();
+        MonsterSetting();
+    }
+
+    @Override
+    public void getThisMonsterImage() {
+        up1 = getImage("/Player/Monster/RedOrc/up1.png");
+        up2 = getImage("/Player/Monster/RedOrc/up2.png");
+        down1 = getImage("/Player/Monster/RedOrc/down1.png");
+        down2 = getImage("/Player/Monster/RedOrc/down2.png");
+        left1 = getImage("/Player/Monster/RedOrc/left1.png");
+        left2 = getImage("/Player/Monster/RedOrc/left2.png");
+        right1 = getImage("/Player/Monster/RedOrc/right1.png");
+        right2 = getImage("/Player/Monster/RedOrc/right2.png");
+    }
+
+    @Override
+    public void MonsterSetting() {
+
+        noticeRange = 4;
+        aggroRange = 9;
+
+        initSpeed(1);
+        maxLife = 17;
+        life = maxLife;
+        dommage = 2;
+    }
+
+}
